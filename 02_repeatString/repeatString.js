32a62be8897f132ffa64d018c3e0ec6a0db376f2
tesseractx0FFFF <itkugit321@gmail.com>
2023-02-23 21:39:40 +0200

    solved the challenge, pseudo code txt file and comments describe the essense of my code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,37 @@
-const repeatString = function() {
+function repeatString(string, num) {
 
+    let inputString = string;
+    let inputNumber = num;
+    
+
+    // check for blank string input.
+    if (inputString === ''){
+         return '';
+        }
+    //    handle input number = 0 or input number negative cases.
+       switch(inputNumber){
+           case 0:
+               return '';
+               
+           case -1:
+               return 'ERROR';
+       }
+    // set a variable to hold future output.
+    let outputString = '';
+    // concatenate the string for (inputNumber - 1) times 
+    for (let i = 0; i <= (inputNumber - 1); i++ ){
+
+        outputString += inputString;
+    }
+
+    // will return string repeated (inputNumber - 1) times.
+
+    return outputString;
 };
+
+repeatString('hey', 3);
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
